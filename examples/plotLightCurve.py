2dd9449c1ed36e484c90dedf262f9d3a367a4b7f
Connor Home <connor.john.crawford@gmail.com>
2024-10-01 23:40:58 +0100

    getting familiar with agp

--- examples/plotLightCurve.py
+++ examples/plotLightCurve.py
@@ -1,24 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import afterglowpy as grb
+import tempfile
 
 # For convenience, place arguments into a dict.
-Z = {'jetType':     grb.jet.TopHat,     # Top-Hat jet
-     'specType':    grb.jet.SimpleSpec, # Basic Synchrotron Emission Spectrum
+Z = {'jetType':     grb.jet.Gaussian,     # Gaussian jet
+     'specType':    grb.jet.SimpleSpec,   # Basic Synchrotron Emission Spectrum
 
-     'thetaObs':    0.05,   # Viewing angle in radians
+     'thetaObs':    0.3,   # Viewing angle in radians
      'E0':          1.0e53, # Isotropic-equivalent energy in erg
-     'thetaCore':   0.1,    # Half-opening angle in radians
-     'n0':          1.0,    # circumburst density in cm^{-3}
+     'thetaCore':   0.05,    # Half-opening angle in radians
+     'thetaWing':   0.4,    # Outer truncation angle
+     'n0':          1.0e-3,    # circumburst density in cm^{-3}
      'p':           2.2,    # electron energy distribution index
      'epsilon_e':   0.1,    # epsilon_e
-     'epsilon_B':   0.01,   # epsilon_B
+     'epsilon_B':   0.0001,   # epsilon_B
      'xi_N':        1.0,    # Fraction of electrons accelerated
-     'd_L':         1.0e28, # Luminosity distance in cm
-     'z':           0.55}   # redshift
+     'd_L':         1.36e26, # Luminosity distance in cm
+     'z':           0.01}   # redshift
 
 # Space time points geometrically, from 10^3 s to 10^7 s
-t = np.geomspace(1.0e3, 1.0e7, 300)
+# Time and Frequencies
+ta = 1.0e-1 * grb.day2sec
+tb = 1.0e3 * grb.day2sec
+t = np.geomspace(ta, tb, num=100)
 
 # Calculate flux in a single X-ray band (all times have same frequency)
 nu = np.empty(t.shape)
@@ -27,11 +32,12 @@ nu[:] = 1.0e18
 # Calculate!
 
 Fnu = grb.fluxDensity(t, nu, **Z)
-
+tday = t * grb.sec2day
 # Write to a file
 
-print("Writing lc.txt")
-with open("lc.txt", 'w') as f:
+filename = tempfile.NamedTemporaryFile(prefix="LightCurve_", suffix=".txt", dir="./data", delete=False)
+print(f'saved to --{filename.name}')
+with open(f'{filename.name}', 'w') as f:
     f.write("# nu " + str(nu) + '\n')
     f.write("# t(s)     Fnu(mJy)\n")
     for i in range(len(t)):
@@ -39,15 +45,16 @@ with open("lc.txt", 'w') as f:
 
 # Plot!
 
-print("Plotting")
+
 fig, ax = plt.subplots(1, 1)
 
-ax.plot(t, Fnu)
+ax.plot(tday, Fnu)
 
 ax.set(xscale='log', xlabel=r'$t$ (s)',
        yscale='log', ylabel=r'$F_\nu$[$10^{18}$ Hz] (mJy)')
 
 fig.tight_layout()
-print("Saving figure lc.png")
-fig.savefig("lc.png")
+graphname = tempfile.NamedTemporaryFile(prefix="LightCurve_", suffix=".png", dir="./graph", delete=False)
+print(f'saved to --{graphname.name}')
+fig.savefig(f'{graphname.name}')
 plt.close(fig)
