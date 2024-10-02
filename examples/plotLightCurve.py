790f61745d48edf6d88b05653709435f8d43afa1
Connor Crawford <cjc233@pypc-genlab-04.campus.bath.ac.uk>
2024-10-02 15:15:08 +0100

    determining the effects of each variable on the flux curves

--- examples/plotLightCurve.py
+++ examples/plotLightCurve.py
@@ -4,10 +4,27 @@ import afterglowpy as grb
 import tempfile
 
 # For convenience, place arguments into a dict.
-Z = {'jetType':     grb.jet.Gaussian,     # Gaussian jet
-     'specType':    grb.jet.SimpleSpec,   # Basic Synchrotron Emission Spectrum
+Z = {'jetType':     grb.jet.Gaussian,     # Gaussian jet your discrepancy
+     'specType':    grb.jet.SimpleSpec,   # Basic Synchrotron Emission Spectrum your discrepancy
+     'counterjet': True,
 
-     'thetaObs':    0.3,   # Viewing angle in radians
+     'thetaObs':    0.3,   # Viewing angle in radians -known
+     'E0':          1.0e53, # Isotropic-equivalent energy in erg
+     'thetaCore':   0.005,    # Half-opening angle in radians
+     'thetaWing':   0.4,    # Outer truncation angle
+     'n0':          1.0e-3,    # circumburst density in cm^{-3}
+     'p':           2.2,    # electron energy distribution index
+     'epsilon_e':   0.1,    # epsilon_e
+     'epsilon_B':   0.0001,   # epsilon_B
+     'xi_N':        1.0,    # Fraction of electrons accelerated
+     'd_L':         1.36e26, # Luminosity distance in cm -known
+     'z':           0.01}   # redshift -known
+     
+Z2 = {'jetType':     grb.jet.Gaussian,     # Gaussian jet your discrepancy
+     'specType':    grb.jet.SimpleSpec,   # Basic Synchrotron Emission Spectrum your discrepancy
+     'counterjet': True,
+
+     'thetaObs':    0.3,   # Viewing angle in radians -known
      'E0':          1.0e53, # Isotropic-equivalent energy in erg
      'thetaCore':   0.05,    # Half-opening angle in radians
      'thetaWing':   0.4,    # Outer truncation angle
@@ -16,8 +33,8 @@ Z = {'jetType':     grb.jet.Gaussian,     # Gaussian jet
      'epsilon_e':   0.1,    # epsilon_e
      'epsilon_B':   0.0001,   # epsilon_B
      'xi_N':        1.0,    # Fraction of electrons accelerated
-     'd_L':         1.36e26, # Luminosity distance in cm
-     'z':           0.01}   # redshift
+     'd_L':         1.36e26, # Luminosity distance in cm -known
+     'z':           0.01}   # redshift -known
 
 # Space time points geometrically, from 10^3 s to 10^7 s
 # Time and Frequencies
@@ -27,11 +44,14 @@ t = np.geomspace(ta, tb, num=100)
 
 # Calculate flux in a single X-ray band (all times have same frequency)
 nu = np.empty(t.shape)
-nu[:] = 1.0e18
+nu[:] = 1.0e18 #x-ray
+nu = np.empty(t.shape)
+nu[:] = 1.0e18 #x-ray
 
 # Calculate!
 
 Fnu = grb.fluxDensity(t, nu, **Z)
+Fnu2 = grb.fluxDensity(t, nu, **Z2)
 tday = t * grb.sec2day
 # Write to a file
 
@@ -49,6 +69,7 @@ with open(f'{filename.name}', 'w') as f:
 fig, ax = plt.subplots(1, 1)
 
 ax.plot(tday, Fnu)
+ax.plot(tday, Fnu2)
 
 ax.set(xscale='log', xlabel=r'$t$ (s)',
        yscale='log', ylabel=r'$F_\nu$[$10^{18}$ Hz] (mJy)')
