2703b201f19a2fda7e0125a40f825475bc600e96
connor laptop <connor.john.crawford@gmail.com>
2024-11-11 14:05:12 +0000

    creating first rough draft fake data with uniform logarithmic spacing in time and spectrum and adding scaling errors for time along with estimiated spectrum error

--- Data_Generation/err_analysis2.py
+++ Data_Generation/err_analysis2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sampler as splr
 
 #unpack data
 time, freq, flux, err = np.genfromtxt('./data/990510.csv',delimiter=',',skip_header=1,unpack=True)
@@ -15,9 +16,12 @@ err_arr = np.full((len(u_time),len(u_freq)), np.nan)
 # Populate the arrays using indices
 flux_arr[time_indices, freq_indices] = flux
 err_arr[time_indices, freq_indices] = err
-print(err_arr)
+#print(len(u_time))
+initial = [0.1,1,2.3,-1,-2,53]
+final_err = [err_arr,np.zeros((len(u_time),len(u_freq))),np.zeros((len(u_time),len(u_freq)))]
+splr.run_optimization([u_time, u_freq],flux_arr, initial, final_err, thetaObs=0.0, xi_N=1.0, d_L=9.461e26, z=1.619)
 
-for freq_idx, nu_value in enumerate(u_freq):
-    plt.errorbar(np.log10(u_time),np.log10(flux_arr[:,freq_idx]),yerr=abs(np.log10(flux_arr[:,freq_idx])*err_arr[:,freq_idx]/flux_arr[:,freq_idx]),fmt='.', label=f'{nu_value:.2e} Hz')
+for freq_idx, nu_value in enumerate(u_freq):                plt.errorbar(np.log10(u_time),np.log10(flux_arr[:,freq_idx]),yerr=abs(err_arr[:,freq_idx]/flux_arr[:,freq_idx]),fmt='.', label=f'{nu_value:.2e} Hz')
 plt.legend()
 plt.show()
+
--- Data_Generation/fake_data_v1.py
+++ Data_Generation/fake_data_v1.py
@@ -12,13 +12,45 @@ from multiprocessing import Pool
 import time
 from functools import partial
 import concurrent.futures
+import sampler as splr
+import random as rnmb
 
+#start by generating a filled out uniform data rectangle over a reasonable bounds
+t = np.geomspace(1e4,1e6,50)
+nu = np.geomspace(1e9,1e20,4)
+x = [t,nu]
+
+F = np.array(splr.Flux(x,0.197,0,2.422,-1,-2,50,0.0,1.0,9.461e26,1.619))
+
+err = np.zeros((len(t),len(nu)))
 
 # try generation in 3 bands x-ray, optical, radio
 #larger errors at larger times
-#x-ray generation error ~ 0.1-0.2
+for t_idx, t_value in enumerate(t):
+    err[t_idx,:] = err[t_idx,:] + t_value/max(t)
 
 #optical generation error ~ 0.02-0.2
 
-#radio generation error ~ 0.1-0.7
+for nu_idx, nu_value in enumerate(nu):
+    #x-ray generation error ~ 0.1-0.2
+    if nu_value > 1e16:
+        err[:,nu_idx] = err[:,nu_idx] + float(rnmb.randint(100,200))/1000
+    #optical UV and Infrared generation error ~ 0.02-0.2
+    if 1e10 < nu_value < 1e16:
+        err[:,nu_idx] = err[:,nu_idx] + float(rnmb.randint(20,200))/1000
+    #radio generation error ~ 0.1-0.7
+    if nu_value < 1e10:
+        err[:,nu_idx] = err[:,nu_idx] + float(rnmb.randint(100,700))/1000
+
+F_err = F + err*rnmb.randint(-1,1)
+
+print(rnmb.randint(-1,1))
+print(rnmb.randint(-1,1))
+print(rnmb.randint(-1,1))
+print(rnmb.randint(-1,1))
+print(rnmb.randint(-1,1))
+for freq_idx, nu_value in enumerate(nu):
+    plt.errorbar(np.log10(t),F[:,freq_idx],yerr=err[:,freq_idx],fmt='.', label=f'{nu_value:.2e} Hz')
+plt.legend()
+plt.show()
 
